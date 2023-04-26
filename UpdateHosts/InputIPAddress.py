2a38579d5e1fa91fdd0b6b9100afefed578b9be4
davidwalgenbach <dwalgenbach@icpms.com>
2023-04-26 10:23:52 -0500

    Add icon

--- UpdateHosts/InputIPAddress.py
+++ UpdateHosts/InputIPAddress.py
@@ -11,6 +11,7 @@ regex = '''^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(
 vartorf = True
 
 win = tk.Tk()
+win.iconbitmap(default='Resources/SampleTRAX Icon.ico')
 win.withdraw()
 #Minsize does not seem to do anything......... WHY
 win.minsize(1200,500)
@@ -18,15 +19,15 @@ win.minsize(1200,500)
 #Input Validation Loop
 while(vartorf == True):
     try:
-        USER_INP = simpledialog.askstring(title="Database Configurator", prompt="Enter the IP Address of the Database PC:")
+        USER_INP = simpledialog.askstring(title="Database Configurator", prompt="Enter the IP Address of the Database PC:                  ")
         if (re.search(regex, USER_INP)):
             vartorf = False
         else:
-            messagebox.showinfo("Error", "Invalid IP Address Format. Please Try again.")
+            messagebox.showerror("Error", "Invalid IP Address Format. Please Try again.")
             vartorf = True
     #Catches exception thrown when input box is immediately closed or cancel is selected TODO: get rid of the cancel button it is useless
     except TypeError:
-        messagebox.showinfo("Error", "No input was given. Please enter a Valid IP Address and try again.")
+        messagebox.showerror("Error", "No input was given. Please enter a Valid IP Address and try again.")
 
 #Adding entry to hosts file
 lines = []
@@ -40,5 +41,5 @@ try:
         for line in lines:
             fp.write(line)
 except PermissionError:
-    messagebox.showinfo("Error", "Administrator permissions are required to run the setup. Please cancel the installation and try again with administrator permissions.")
+    messagebox.showerror("Error", "Administrator permissions are required to run the setup. Please cancel the installation and try again with administrator permissions.")
     exit
